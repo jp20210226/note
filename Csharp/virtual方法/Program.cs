c0d42b09809be2a4c7978d2af3abfec3f3a77f5c
jp20210226 <465277271@qq.com>
2021-04-26 13:42:26 +0800

    practice

---
+++ Csharp/virtual方法/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace virtual方法
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+            a.Say();    //SayB
+            a.Speak();  //SpeakA
+
+            B b = new B();
+            b.Say();    //SayB
+            b.Speak();  //SpeakB
+
+            Console.ReadLine();
+        }
+    }
+ 
+    public class A
+    {
+        public virtual void Say()
+        {
+            Console.WriteLine("SayA");
+        }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("SpeakA");
+        }
+    }
+
+    public class B : A
+    {
+        public void OnlyB()
+        {
+
+        }
+        public override void Say()
+        {
+            Console.WriteLine("SayB");
+        }
+
+        public  void Speak()
+        {
+            Console.WriteLine("SpeakB");
+        }
+    }
+   
+
+    public class Parent
+    {
+        protected readonly string name = "wedq";
+
+        public Parent()
+        {
+            Console.WriteLine(GetNamelen());
+        }
+        public virtual int GetNamelen()
+        {
+            return 0;
+        }
+    }
+
+    public class Child : Parent
+    {
+        public override  int GetNamelen()
+        {
+            return base.name.Length; 
+        }
+    }
+
+}
---
+++ Csharp/枚举/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace 枚举
+{
+    public enum Common 
+    {
+        None = 0,    
+        Left = 1,    
+        Right = 2,   
+        Top = 3,     
+        Bottom = 4  
+    }
+
+    /// <summary>
+    /// 最多到255
+    /// </summary>
+    public enum Test:byte
+    {
+        None = 0,   //0000 0000
+        Left = 1,   //0000 0001
+        Right = 2,  //0000 0010
+        Top = 3,    //0000 0100
+        Bottom = 4 //0000 1000
+    } 
+
+    [Flags]
+    /*
+    1、按约定,如果枚举成员可组合的话,fags属性就应该应用在枚举类型上
+    如果声明了这样的枚举却没有使用fags属性,你仍然可以组合枚举的成员,但是
+    调用枚举实例的 ToString0方法时,输出的将是一个数值而不是一组名称。
+    按约定,可组合枚举的名称应该是复数
+    2、在声明可组合枚举的时候,可以直接使用组合的枚举成员作为成员
+    */
+    public enum BorderSides
+    {
+        None =0,   //0000 0000
+        Left =1,   //0000 0001
+        Right =2,  //0000 0010
+        Top =4,    //0000 0100
+        Bottom = 8, //0000 1000
+        LeftRight = Left |  Right,
+        TopBottom = Top| Bottom,
+        All= LeftRight| TopBottom
+    } 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BorderSides leftright = BorderSides.Left | BorderSides.Right;
+            Console.WriteLine(leftright.ToString());
+
+            var b = BorderSides.Top | BorderSides.Right | BorderSides.Bottom;
+            /*
+            Top         0000 0100
+            Right       0000 0010
+            Bottom      0000 1000
+            ---------------------
+            b:          0000 1110
+            */
+
+            Console.WriteLine(b.ToString());
+            var c = b & BorderSides.Right;
+            /*
+            b           0000 1110
+            Right       0000 0010
+            ---------------------
+            b:          0000 0010
+            */
+            Console.WriteLine(c.ToString());
+
+
+            //检查枚举值的合理性: Enum.dEfined)静态方法
+            Common test = (Common) 12345;
+            Console.WriteLine(Enum.IsDefined(typeof(Test), test)); // False
+        }
+    }
+}
