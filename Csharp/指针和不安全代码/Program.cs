33166d49672031baf6f27225bba08f16c0b2f18c
jp20210226 <465277271@qq.com>
2021-05-11 10:44:00 +0800

    指针和不安全

---
+++ Csharp/指针和不安全代码/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace 指针和不安全代码
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test1();
+        }
+
+        static unsafe void BlueFilter(int[,] bitmap)
+        {
+            int length = bitmap.Length;
+            fixed (int* b = bitmap)
+            {
+                int* p = b;
+                for (int i = 0; i < length; i++)
+                {
+                    *p++ &= 0xFF;
+                }
+            }
+
+
+        }
+
+        static unsafe void test1()
+        {
+            int* a = stackalloc int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
